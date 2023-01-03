5c8f978cd799a7615645256bdcd2ad49acc12fa7
Ed <pselok@gmail.com>
2023-01-03 04:39:26 +0400

    keychain fix

--- Sources/CoreKit/Keychain/Keychain.swift
+++ Sources/CoreKit/Keychain/Keychain.swift
@@ -3,13 +3,13 @@ import KeychainAccess
 
 public struct Keychain {
     public static func save(_ key: String, for wallet: Wallet) throws {
-        let keychain = KeychainAccess.Keychain(service: Service.wallets.value)
+        let keychain = KeychainAccess.Keychain(service: Service.wallets.service(for: .icloud))
             .synchronizable(true)
         try keychain
             .set(key, key: wallet.id)
     }
     public static func save(_ wallet: Wallet) throws {
-        let keychain = KeychainAccess.Keychain(service: Service.wallets.value)
+        let keychain = KeychainAccess.Keychain(service: Service.wallets.service(for: wallet.location.synchronizable ? .icloud : .device))
             .synchronizable(wallet.location.synchronizable)
         try keychain
             .label(wallet.title)
@@ -17,11 +17,20 @@ public struct Keychain {
             .set(wallet.phrase, key: wallet.id)
     }
     public static func delete(_ wallet: Wallet) throws {
-        try KeychainAccess.Keychain(service: Service.wallets.value).remove(wallet.id)
-        try KeychainAccess.Keychain(service: Service.keys.value).remove(wallet.id)
+        try KeychainAccess.Keychain(service: Service.wallets.service(for: .device)).remove(wallet.id)
+        try KeychainAccess.Keychain(service: Service.wallets.service(for: .icloud)).remove(wallet.id)
+        try KeychainAccess.Keychain(service: Service.keys.service(for: .device)).remove(wallet.id)
+        try KeychainAccess.Keychain(service: Service.keys.service(for: .icloud)).remove(wallet.id)
     }
-    public static func wallet(by id: String) -> Wallet? {
-        let keychain = KeychainAccess.Keychain(service: Service.wallets.value)
+    public static func wallets() -> [Wallet] {
+        let device = KeychainAccess.Keychain(service: Service.wallets.service(for: .device))
+        let icloud = KeychainAccess.Keychain(service: Service.wallets.service(for: .icloud))
+        let locals = device.allKeys().compactMap({wallet(by: $0, location: .device)})
+        let remote = icloud.allKeys().compactMap({wallet(by: $0, location: .icloud)})
+        return locals + remote
+    }
+    public static func wallet(by id: String, location: Keychain.Location) -> Wallet? {
+        let keychain = KeychainAccess.Keychain(service: Service.wallets.service(for: location))
         guard let phrase = keychain[id],
               let attributes = keychain[attributes: id],
               let comments = attributes.comment,
@@ -36,10 +45,6 @@ public struct Keychain {
                       created: Core.Date(with: Date(timeIntervalSince1970: date)),
                       location: .keychain(icloud ? .icloud : .device))
     }
-    public static func wallets() -> [Wallet] {
-        let keychain = KeychainAccess.Keychain(service: Service.wallets.value)
-        return keychain.allKeys().compactMap({wallet(by: $0)})
-    }
 }
 
 extension Keychain {
@@ -48,11 +53,24 @@ extension Keychain {
         case wallets
         case passcode
         
-        public var value: String {
+        fileprivate var device: String {
             switch self {
-            case .keys: return "bar.multi.wallet.keys"
-            case .wallets: return "bar.multi.wallet.wallets"
-            case .passcode: return "bar.multi.wallet.passcode"
+            case .keys: return "bar.multi.wallet.device.keys"
+            case .wallets: return "bar.multi.wallet.device.wallets"
+            case .passcode: return "bar.multi.wallet.device.passcode"
+            }
+        }
+        fileprivate var icloud: String {
+            switch self {
+            case .keys: return "bar.multi.wallet.icloud.keys"
+            case .wallets: return "bar.multi.wallet.icloud.wallets"
+            case .passcode: return "bar.multi.wallet.icloud.passcode"
+            }
+        }
+        fileprivate func service(for location: Keychain.Location) -> String {
+            switch location {
+            case .device: return device
+            case .icloud: return icloud
             }
         }
     }
